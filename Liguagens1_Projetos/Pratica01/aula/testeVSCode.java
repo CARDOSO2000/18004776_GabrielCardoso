8493a3a617dbb26a9a8e903b02873bb2ec75e660
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-03-02 12:52:40 -0300

    Oi

---
+++ Liguagens1_Projetos/Pratica01/aula/testeVSCode.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+class testeVSCode{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ola mundo:");
+        String nome;
+        nome = scanner;
+        System.out.println(nome);
+    }
+}
