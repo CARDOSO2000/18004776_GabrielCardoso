92c8ed2a387e034bc739745912acb4931df8eee3
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-03-26 09:35:30 -0300

    Atividades

---
+++ Liguagens1_Projetos/Respostalista2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+           Jogo l = new Jogo();
+           l.jogar();
+
+        }
+
+}
