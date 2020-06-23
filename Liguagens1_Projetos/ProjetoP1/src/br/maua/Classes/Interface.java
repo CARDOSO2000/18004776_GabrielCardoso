6a68c953d3beb182bb53ac44d313f613ee79f555
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-06-23 13:36:55 -0300

    Criação e Implementação da classe Interface

---
+++ Liguagens1_Projetos/ProjetoP1/src/br/maua/Classes/Interface.java
@@ -0,0 +1,11 @@
+package br.maua.Classes;
+
+public class Interface {
+    public void Interface(){
+        System.out.println("Pizzaria o Rato que Ri:");
+        System.out.println("1- Nova Venda");
+        System.out.println("2- Verificar pedidos");
+        System.out.println("3- Alterar pedidos");
+        System.out.println("0- Sair");
+    }
+}
