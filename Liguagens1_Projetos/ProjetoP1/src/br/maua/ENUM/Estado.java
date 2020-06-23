d6423c98a1d6aac06076826089e7953b23bd9ed0
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-06-23 13:32:35 -0300

    Termino do Pagamento e criação do Estado

---
+++ Liguagens1_Projetos/ProjetoP1/src/br/maua/ENUM/Estado.java
@@ -0,0 +1,4 @@
+package br.maua.ENUM;
+
+public enum Estado {
+}
--- Liguagens1_Projetos/ProjetoP1/src/br/maua/ENUM/Paagamento.java
+++ Liguagens1_Projetos/ProjetoP1/src/br/maua/ENUM/Paagamento.java
@@ -1,5 +1,5 @@
 package br.maua.ENUM;
 
 public enum Paagamento {
-
+    DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO;
 }
