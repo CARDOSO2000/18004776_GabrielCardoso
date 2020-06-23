ac91e5e39f393fdc6ced82c0d5082f028bd05413
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-06-23 16:29:12 -0300

    Implemantando usuario e criação de pedido

--- Liguagens1_Projetos/ProjetoP1/src/br/maua/Classes/Usuario.java
+++ Liguagens1_Projetos/ProjetoP1/src/br/maua/Classes/Usuario.java
@@ -1,4 +1,25 @@
 package br.maua.Classes;
 
 public class Usuario {
+    private String Name;
+    private String senha;
+    private String email;
+
+    public Usuario(String name, String senha, String email) {
+        Name = name;
+        this.senha = senha;
+        this.email = email;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
