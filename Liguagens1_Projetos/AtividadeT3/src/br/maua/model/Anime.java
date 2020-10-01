a8161563aaad18bfb1517fad575b0d76ac6f1040
CARDOSO2000 <gdlaurentis.diascardoso@gmail.com>
2020-10-01 11:50:57 -0300

    Crição de metodos no anime e criação do Parser

--- Liguagens1_Projetos/AtividadeT3/src/br/maua/model/Anime.java
+++ Liguagens1_Projetos/AtividadeT3/src/br/maua/model/Anime.java
@@ -1,4 +1,48 @@
 package br.maua.model;
 
 public class Anime {
+    private String nome;
+    private String url;
+    private String sinopse;
+    private float nota;
+    private int episodio;
+
+    public Anime(String nome, String url, String sinopse, float nota, int episodio) {
+        this.nome = nome;
+        this.url = url;
+        this.sinopse = sinopse;
+        this.nota = nota;
+        this.episodio = episodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public int getEpisodio() {
+        return episodio;
+    }
+
+    @Override
+    public String toString() {
+        return "Anime{" +
+                "nome='" + nome + '\'' +
+                ", url='" + url + '\'' +
+                ", sinopse='" + sinopse + '\'' +
+                ", nota=" + nota +
+                ", episodio=" + episodio +
+                '}';
+    }
 }
